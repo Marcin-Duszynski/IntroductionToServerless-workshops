ddf53eba3d7816b7caf42515c73f4067dd131e43
Marcin-Duszynski <m.duszynski@kainos.com>
2018-07-25 08:26:27 +0200

    ng route tweak

--- WebApp/src/app/app.routes.ts
+++ WebApp/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core";
 import { LogoutComponent } from "./logout/logout.component";
 import { SearchComponent } from './search/search.component';
+import { CanActivateLogged } from './shared/routing/canActivateLogged';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LogoutComponent },
   { path: 'logout', component: LogoutComponent },
-  { path: 'search', component: SearchComponent },
+  { path: 'search', component: SearchComponent, canActivate: [CanActivateLogged] },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
