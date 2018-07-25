bb17e2a88dc9f33c41662332177c07bde1affaf9
Marcin-Duszynski <m.duszynski@kainos.com>
2018-07-25 08:24:59 +0200

    code cleanup

--- WebApp/src/app/app.routes.ts
+++ WebApp/src/app/app.routes.ts
@@ -2,13 +2,12 @@ import { RouterModule, Routes } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core";
 import { LogoutComponent } from "./logout/logout.component";
 import { SearchComponent } from './search/search.component';
-import { CanActivateLogged } from "./shared/routing/canActivateLogged";
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LogoutComponent },
   { path: 'logout', component: LogoutComponent },
-  { path: 'search', component: SearchComponent, canActivate: [CanActivateLogged] },
+  { path: 'search', component: SearchComponent },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
